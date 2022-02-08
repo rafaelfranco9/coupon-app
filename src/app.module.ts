cee9a6d3723390aa4b225030214fa0d261953045
Rafael Franco <francosprivato@gmail.com>
2022-02-08 20:14:52 -0300

    deleting template files that are out of use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CouponsController } from './coupons/coupons.controller';
 import { CouponsService } from './coupons/coupons.service';
 import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
-  controllers: [AppController, CouponsController],
-  providers: [AppService, CouponsService, ProductsService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
+    HttpModule,
+  ],
+  controllers: [CouponsController],
+  providers: [CouponsService, ProductsService],
 })
 export class AppModule {}
